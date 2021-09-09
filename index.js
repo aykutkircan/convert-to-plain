8022c7fc6377397fdc87161fee0f239a7c173148
Aykut Kırcan <aykutkircan@icloud.com>
2021-09-09 17:37:22 +0300

    fix(test): cahnge replace parameters

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ convertToPlain = (html) => {
         return "";
     }
 
-    const strippedHtml = html.replace(/(\t)|(<[^>]+>)/g, "");
+    const strippedHtml = html
+        .replace(/\n/ig, '')
+        .replace(/<style[^>]*>[\s\S]*?<\/style[^>]*>/ig, '')
+        .replace(/<head[^>]*>[\s\S]*?<\/head[^>]*>/ig, '')
+        .replace(/<script[^>]*>[\s\S]*?<\/script[^>]*>/ig, '')
+        .replace(/<\/\s*(?:p|div)>/ig, '\n')
+        .replace(/<br[^>]*\/?>/ig, '\n')
+        .replace(/<[^>]*>/ig, '')
+        .replace('&nbsp;', ' ')
+        .replace(/[^\S\r\n][^\S\r\n]+/ig, ' ')
 
     return strippedHtml || "";
 };
